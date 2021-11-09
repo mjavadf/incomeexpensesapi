be33fe3aa1f20b74716f51c3b01e884c8e50825c
Your Name <you@example.com>
2021-11-09 16:35:08 +0330

    some changes on user model and admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'username',
+        'email',
+        'is_verified',
+        'is_active',
+        'is_superuser',
+        'is_staff',
+        'created_at',
+        'updated_at',]
