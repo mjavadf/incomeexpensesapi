d4bba482a036caa723c83b156c3999d62cb0e87c
Your Name <you@example.com>
2021-11-10 22:45:02 +0330

    install drf_yasg and use it for create documentation

--- authentication/views.py
+++ authentication/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.sites.shortcuts import get_current_site
 from django.conf import settings
-from rest_framework import generics, status
+from rest_framework import generics, status, views
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, EmailVerificationSerializer
 from .utils import Util
 import jwt
 
@@ -39,7 +41,17 @@ class RegisterView(generics.GenericAPIView):
         
         return Response(user_data, status=status.HTTP_201_CREATED)
     
-class VerifyEmail(generics.GenericAPIView):
+class VerifyEmail(views.APIView):
+    serializer_class = EmailVerificationSerializer
+   
+    token_param_config = openapi.Parameter(
+        'token', 
+        in_=openapi.IN_QUERY,
+        description='Description',
+        type=openapi.TYPE_STRING
+    )
+   
+    @swagger_auto_schema(manual_parameters=[token_param_config])
     def get(self, request):
         token = request.GET.get('token')
         try:
