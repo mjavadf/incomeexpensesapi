6d8aec5659b01c2c1cf02c9cf437a4a04e93e34a
Your Name <you@example.com>
2021-11-10 21:10:15 +0330

    complete email verification

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.sites.shortcuts import get_current_site
+from django.conf import settings
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 from .serializers import RegisterSerializer
 from .utils import Util
+import jwt
 
 class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
@@ -32,10 +34,24 @@ class RegisterView(generics.GenericAPIView):
             'email_body': email_body,
             'email_subject': 'Verify Your Account',
         }
+        
         Util.send_email(data)
         
         return Response(user_data, status=status.HTTP_201_CREATED)
     
 class VerifyEmail(generics.GenericAPIView):
-    def get(self):
-        pass
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms='HS256')
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'messages': 'Account successfully verified'}, status=status.HTTP_200_OK)
+            
+        except jwt.ExpiredSignatureError:
+            return Response({'messages': 'Activation link is expired'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except jwt.exceptions.DecodeError:
+            return Response({'messages': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
